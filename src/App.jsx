0c18ed754def208bf40a0d394bb427f8c08bb068
Anton-Osip <osipchika@gmail.com>
2025-04-22 11:02:49 +0300

    feat: Dialogs with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
-        <div className = "app-wrapper">
-            <Header/>
-            <Navbar/>
-            <Profile/>
-        </div>
-    );
+        <BrowserRouter>
+            <div className = "app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className = "app-wrapper-content">
+                    <Route path = "/dialogs" component = {Dialogs}/>
+                    <Route path = "/profile" component = {Profile}/>
+                </div>
+            </div>
+        </BrowserRouter>)
 }
 
 export default App;
